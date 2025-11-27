ce391e7e86d916c577eb0d80db04d3f1fb039a86
Далер <dsafoev40@gmail.com>
2025-11-27 19:41:47 +0300

    Первый коммит

---
+++ src/main/java/ru/safoev/gum/GumSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.safoev.gum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/safoev/gum/GumSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safoev.gum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

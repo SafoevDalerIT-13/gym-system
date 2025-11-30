a6b97950f5248fe512cf73c91f0f12d7a0d11533
Далер <dsafoev40@gmail.com>
2025-11-30 15:31:08 +0300

    Добавил обработчик ошибок

---
+++ src/main/java/ru/safoev/foundexception/ErrorResponseDto.java
@@ -0,0 +1,10 @@
+package ru.safoev.foundexception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponseDto(
+        String message,
+        String detailedMessage,
+        LocalDateTime errorTime
+) {
+}
